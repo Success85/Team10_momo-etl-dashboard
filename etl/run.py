de4ea3bdbe291afc82d24211cd8ac7afbec31a7b
Success85 <s.ituma@alustudent.com>
2026-05-26 18:29:02 +0200

    Merged with Natty's work set up database connection and path

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from config import BATCH_SIZE, INITIALIZE_DB, LOG_PATH, OUTPUT_PATH, XML_PATH
 from load_db import import_sms_to_database
 
@@ -15,3 +16,16 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+=======
+import sqlite3
+import os
+
+DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'momo.db')
+
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
+>>>>>>> aa31213 (set up database connection and path)
