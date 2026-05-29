ea58c5a41b87e87b78090e99c478d70fc2884c79
Success85 <s.ituma@alustudent.com>
2026-05-29 22:00:37 +0200

    Connect do_POST to SQLite database

--- etl/run.py
+++ etl/run.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from config import BATCH_SIZE, INITIALIZE_DB, LOG_PATH, OUTPUT_PATH, XML_PATH
 from load_db import import_sms_to_database
 
@@ -16,16 +15,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-=======
-import sqlite3
-import os
-
-DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'momo.db')
-
-
-def get_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON")
-    return conn
->>>>>>> aa31213 (set up database connection and path)
